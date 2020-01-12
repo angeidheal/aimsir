6efb843ba8081a57e023e33117bc1f28c13e5ba4
Crìstean MacMhìcheil <MacMhicheil@users.noreply.github.com>
2020-01-12 19:34:44 +0000

    Update aimsir.js

--- aimsir.js
+++ aimsir.js
@@ -11,8 +11,8 @@ window.addEventListener('load', ()=> {
         navigator.geolocation.getCurrentPosition(position => {
             long = position.coords.longitude;
             lat = position.coords.latitude;
-
-            const api = `https://api.darksky.net/forecast/9e00d522da20f32ddb9cc5f341b5338a/${lat},${long}`;
+            const proxy = 'https://cors-anywhere.herokuapp.com/';
+            const api = `${proxy}https://api.darksky.net/forecast/9e00d522da20f32ddb9cc5f341b5338a/${lat},${long}`;
             fetch(api)
                 .then(response =>{
                     return response.json();
