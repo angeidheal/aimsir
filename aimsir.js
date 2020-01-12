3a60b62881de1f44e9a525ca38230708ded36910
Crìstean MacMhìcheil <MacMhicheil@users.noreply.github.com>
2020-01-12 21:58:08 +0000

    Update aimsir.js

--- aimsir.js
+++ aimsir.js
@@ -22,7 +22,7 @@ window.addEventListener('load', ()=> {
                     // Set DOM Elements from the API
                     degree.textContent = temperature  + " °C";
                     currentSummary.textContent = summary;
-                    dailySummary.textContent = daily.data[1].summary;
+                    dailySummary.textContent = .data.daily.summary;
                     // Set icon
                     setIcons(icon, document.querySelector(".icon"));
 
