324e4a88af3e7e6bab137eaf4f215d5bbbee467d
Chris Mitchell <MacMhicheil@users.noreply.github.com>
2020-01-13 12:00:14 +0000

    Update aimsir.js

--- aimsir.js
+++ aimsir.js
@@ -43,8 +43,8 @@ window.addEventListener('load', ()=> {
                     degree.textContent = temperature  + " °C";
                     currentSummary.textContent = summary;
 
-                    var formattedTime1 = new Date1(data.daily.data[1].time * 1000)
-                    var day1 = date1.getDay();
+                    var formattedTime1 = new Date(data.daily.data[1].time * 1000)
+                    var day1 = date.getDay();
                     var formattedTime1 = day1;
                     console.log(formattedTime1);
 
