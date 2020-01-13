991bcd52c2a0c8de91fe4860cfcf2ebd4d119434
Chris Mitchell <MacMhicheil@users.noreply.github.com>
2020-01-13 09:15:58 +0000

    Update aimsir.js

--- aimsir.js
+++ aimsir.js
@@ -43,13 +43,13 @@ window.addEventListener('load', ()=> {
                     degree.textContent = temperature  + " °C";
                     currentSummary.textContent = summary;
 
-                    dailyTemp1.textContent = data.daily.data[1].temperaturehigh;
-                    dailyTemp2.textContent = data.daily.data[2].temperaturehigh;
-                    dailyTemp3.textContent = data.daily.data[3].temperaturehigh;
-                    dailyTemp4.textContent = data.daily.data[4].temperaturehigh;
-                    dailyTemp5.textContent = data.daily.data[5].temperaturehigh;
-                    dailyTemp6.textContent = data.daily.data[6].temperaturehigh;
-                    dailyTemp7.textContent = data.daily.data[7].temperaturehigh;
+                    dailyTemp1.textContent = data.daily.data[1].temperatureHigh;
+                    dailyTemp2.textContent = data.daily.data[2].temperatureHigh;
+                    dailyTemp3.textContent = data.daily.data[3].temperatureHigh;
+                    dailyTemp4.textContent = data.daily.data[4].temperatureHigh;
+                    dailyTemp5.textContent = data.daily.data[5].temperatureHigh;
+                    dailyTemp6.textContent = data.daily.data[6].temperatureHigh;
+                    dailyTemp7.textContent = data.daily.data[7].temperatureHigh;
 
                     dailySummary1.textContent = data.daily.data[1].summary;
                     dailySummary2.textContent = data.daily.data[2].summary;
