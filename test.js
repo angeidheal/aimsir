1b6d637252dbb5256e5ac67e3dbfc327390f0f99
Chris Mitchell <MacMhicheil@users.noreply.github.com>
2020-03-08 18:23:48 +0000

    Update test.js

--- test.js
+++ test.js
@@ -113,8 +113,8 @@ window.addEventListener('load', ()=> {
                     var formattedDate7 = days[date.getDay()];
                     console.log(formattedDate7)
                     }
-
-                    const currentLocation = 'https:\//maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${long}&location_type=ROOFTOP&result_type=street_address&key=AIzaSyAvcFZLm-pJBxUliIdp0-Y2hmI8p02Wh1A';
+                  )
+                    const currentLocation = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${long}&location_type=ROOFTOP&result_type=street_address&key=AIzaSyAvcFZLm-pJBxUliIdp0-Y2hmI8p02Wh1A';
                       fetch(currentLocation)
                         .then(city =>{
                           console.log(city);
