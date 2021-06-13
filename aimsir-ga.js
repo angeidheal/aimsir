5678ba7b6993d5849eccd28b9e42b74c6fd9e51d
Chris Mitchell <MacMhicheil@users.noreply.github.com>
2021-06-13 12:09:46 +0100

    Update aimsir-ga.js

--- aimsir-ga.js
+++ aimsir-ga.js
@@ -93,7 +93,7 @@ window.addEventListener('load', ()=> {
         navigator.geolocation.getCurrentPosition(position => {
             long = position.coords.longitude;
             lat = position.coords.latitude;
-            const proxy = 'https://cors-anywhere.herokuapp.com/';
+            const proxy = 'http://alloworigin.com/get?url=';
             const api = `${proxy}https://api.darksky.net/forecast/9e00d522da20f32ddb9cc5f341b5338a/${lat},${long}?lang=ga&units=uk2`;
             fetch(api)
                 .then(response =>{
